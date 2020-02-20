401052e1bf9934668e47ee8b1e1e9ea996b6bf6b
GregWagner <theGreg101@gmail.com>
2020-02-20 14:25:33 -0600

    Redid 11727

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/11727-Cost_Cuttng/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/11727-Cost_Cuttng/main.cpp
@@ -1,26 +1,26 @@
 /*
- * Problem 11727 - Cost Cutting
+ * 11727 - Cost Curring
+ * Sort the 3 numbers and get the median
  */
 #include <iostream>
 #include <sstream>
+#include <array>
+#include <algorithm>
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::array<int, 3> a;
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int test = 1;
-
-    int testCases;
-    std::cin >> testCases;
-    while (testCases--) {
-        int first, second, third;
-        std::cin >> first >> second >> third;
-        int total = first + second + third;
-        output << "Case " << test++ << ": " <<
-            total - std::min(first, std::min(second, third))
-            - std::max(first, std::max(second, third)) << '\n';
-    }
-
-    std::cout << output.str();
+  std::ostringstream output;
+  int testCases {};
+  std::cin >> testCases;
+  int test {1};
+  while (testCases--) {
+    std::cin >> a[0] >> a[1] >> a[2];
+    std::sort(a.begin(), a.end());
+    output << "Case " << test++ << ": " << a[1] << '\n';
+  }
+  std::cout << output.str();
 }
