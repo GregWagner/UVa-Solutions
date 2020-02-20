534968ca2b331e3d87e15a8040119bcb5f777218
GregWagner <theGreg101@gmail.com>
2020-02-20 15:20:28 -0600

    Redo 12372

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/12372-Packing_for_Holiday/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/12372-Packing_for_Holiday/main.cpp
@@ -1,25 +1,26 @@
 /*
- * Problem 12372 - Packing for Holiday
+ * UVa 12372 - Packing for Holiday
+ * Just check if all L, W, H <= 20
  */
 #include <iostream>
 #include <sstream>
 
-int main() {
-    constexpr int SIZE {20};
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  const int SIZE {20};
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int test {1};
-
-    int testCases;
-    std::cin >> testCases;
-    while (testCases--) {
-        int length, width, height;
-        std::cin >> length >> width >> height;
-        output <<  "Case " << test++ << ": "
-            << (length <= SIZE && width <= SIZE && height <= SIZE ?
-                "good\n" : "bad\n");
-    }
-    std::cout << output.str();
+  std::ostringstream output;
+  int testCases {};
+  std::cin >> testCases;
+  int test {1};
+  while (testCases--) {
+    int length {}, width {}, height {};
+    std::cin >> length >> width >> height;
+    output << "Case " << test++ << ": "
+      << (length <= SIZE && width <= SIZE && height <= SIZE
+          ? "good" : "bad") << '\n';
+  }
+  std::cout << output.str();
 }
