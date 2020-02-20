36698e66a02b0e97e856b95d7c0f44778628bc46
GregWagner <theGreg101@gmail.com>
2020-02-20 09:08:35 -0600

    Redid 1124

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/1124-Celebrity_Jeopardy/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/1124-Celebrity_Jeopardy/main.cpp
@@ -1,20 +1,20 @@
 /*
- * Problem 1124 - Celebrity Jeopardy
- *
- * Echos the input to the output
+ * UVa 01124 - Celebtiry Jeopardy
+ * Just echo/re-print the input again
  */
 #include <iostream>
 #include <sstream>
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    std::string input;
+  std::ostringstream output;
+  std::string input;
 
-    while (std::getline(std::cin, input)) {
-        output << input << '\n';
-    }
-    std::cout << output.str();
+  while (std::getline(std::cin, input)) {
+    output << input << '\n';
+  }
+  std::cout << output.str();
 }
