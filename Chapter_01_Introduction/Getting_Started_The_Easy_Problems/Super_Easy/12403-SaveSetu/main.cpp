d3423286005fcfd0de61e2b99ba71302d83c5ff7
GregWagner <theGreg101@gmail.com>
2020-02-20 15:01:22 -0600

    Redo 12403

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/12403-SaveSetu/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Super_Easy/12403-SaveSetu/main.cpp
@@ -1,27 +1,29 @@
 /*
- * Problem 12403 Save Setu
+ * UVa 12403 - Save Setu
+ * Straightforward
  */
 #include <iostream>
 #include <sstream>
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int balance {};
-    int testCases;
-    std::cin >> testCases;
-    while (testCases--) {
-        std::string cmd;
-        std::cin >> cmd;
-        if (cmd == "donate") {
-            int amount;
-            std::cin >> amount;
-            balance += amount;
-        } else {
-            output << balance << '\n';
-        }
+  std::ostringstream output;
+  int amount {};
+  int testCases {};
+  std::cin >> testCases;
+  while (testCases--) {
+    std::string input;
+    std::cin >> input;
+    if (input == "donate") {
+      int donation {};
+      std::cin >> donation;
+      amount += donation;
+    } else {
+      output << amount << '\n';
     }
-    std::cout << output.str();
+  }
+  std::cout << output.str();
 }
