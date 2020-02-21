e8ff34f477c08200b23c79dd0ffb0b98028b3284
GregWagner <theGreg101@gmail.com>
2020-02-21 16:44:10 -0600

    Redo 11332

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/11332-Summing_Digits/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/11332-Summing_Digits/main.cpp
@@ -1,29 +1,32 @@
 /*
- * Problem 11332 Summing Digits
+ * UVa 11332 - Summing Digits
+ * Simple recursion
  */
 #include <iostream>
 #include <sstream>
 
-int sumDigits(int number) {
-    int sum {};
-    while (number > 0) {
-        sum += number % 10;
-        number /= 10;
-    }
-    return sum;
+int addDigits(int number)
+{
+  int results {};
+  while (number) {
+    results += number % 10;
+    number /= 10;
+  }
+  return results;
 }
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int number;
-    while (std::cin >> number && number != 0) {
-        while (number >= 10) {
-            number = sumDigits(number);
-        }
-        output << number << '\n';
+  std::ostringstream output;
+  int number {};
+  while (std::cin >> number && number != 0) {
+    while (number >= 10) {
+      number = addDigits(number);
     }
-    std::cout << output.str();
+    output << number << '\n';
+  }
+  std::cout << output.str();
 }
