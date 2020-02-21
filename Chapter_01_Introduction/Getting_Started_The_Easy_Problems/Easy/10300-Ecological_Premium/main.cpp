833a0398b19093b698da4d433eaeb18bd531b6e8
GregWagner <theGreg101@gmail.com>
2020-02-21 16:08:56 -0600

    Redo 10300

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/10300-Ecological_Premium/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/10300-Ecological_Premium/main.cpp
@@ -1,32 +1,33 @@
 /*
- * Problem 10300 - Ecological Premium
+ * UVa 10300 - Ecological Premium
+ * Ignore the number of animals
  *
  * space per animal * environment_friendliness * number of animals
  * note: number of animals factors out
- *
  */
 #include <iostream>
 #include <sstream>
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int testCases;
-    std::cin >> testCases;
-    while (testCases--) {
-        int burden {};
-        int numberOfFarmers;
-        std::cin >> numberOfFarmers;
-        for (int i {}; i < numberOfFarmers; ++i) {
-            int size;
-            int numberOfAnimals;
-            int environmentFriendliness;
-            std::cin >> size >> numberOfAnimals >> environmentFriendliness;
-            burden += (size * environmentFriendliness);
-        }
-        output << burden << '\n';
+  std::ostringstream output;
+  int testCases {};
+  std::cin >> testCases;
+  while (testCases--) {
+    int numberOfFarmers {};
+    std::cin >> numberOfFarmers;
+
+    int burden {};
+    for (int i {}; i < numberOfFarmers; ++i) {
+      int size {}, numberOfAnimals {}, value {};
+      std::cin >> size >> numberOfAnimals >> value;
+
+      burden += size * value;
     }
-    std::cout << output.str();
+    output << burden << '\n';
+  }
+  std::cout << output.str();
 }
