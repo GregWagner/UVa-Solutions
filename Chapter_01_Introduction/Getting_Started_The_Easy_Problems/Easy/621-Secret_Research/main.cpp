19602c6782a55a2fcb154ec3f910ee5973249e34
GregWagner <theGreg101@gmail.com>
2020-02-21 08:33:17 -0600

    Redo 621

--- Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/621-Secret_Research/main.cpp
+++ Chapter_01_Introduction/Getting_Started_The_Easy_Problems/Easy/621-Secret_Research/main.cpp
@@ -1,29 +1,31 @@
 /*
- * Problem 621 - Secret Research
+ * Uva 00621 - Secret Research
+ * Case analysis for only 4 possible outputs
  */
 #include <iostream>
 #include <sstream>
 
-int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    std::ostringstream output;
-    int testCases;
-    std::cin >> testCases;
-    while (testCases--) {
-        std::string input;
-        std::cin >> input;
-        int size = input.size() - 1;
-        if (input == "1" || input == "4" || input == "78") {
-            output << "+\n";
-        } else if (input[size - 1] == '3' && input[size] == '5') {
-            output << "-\n";
-        } else if (input[0] == '9' && input[size] == '4') {
-            output << "*\n";
-        } else {
-            output << "?\n";
-        }
+  std::ostringstream output;
+  int testCases {};
+  std::cin >> testCases;
+  while (testCases--) {
+    std::string input {};
+    std::cin >> input;
+    const int size = input.size() - 1;
+    if (input == "1" || input == "4" || input == "78") {
+      output << "+\n";
+    } else if (size > 1 && input[size - 1] == '3' && input[size] == '5') {
+      output << "-\n";
+    } else if (size > 1 && input[0] == '9' && input[size] == '4') {
+      output << "*\n";
+    } else {
+      output << "?\n";
     }
-    std::cout << output.str();
+  }
+  std::cout << output.str();
 }
